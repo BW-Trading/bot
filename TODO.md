2b535ca1c8c375c8f50938046e2fa8ff64522533
William Isabelle <william.isabelle@etu.univ-grenoble-alpes.fr>
2025-02-17 22:27:15 +0100

    add linter config file

---
+++ TODO.md
@@ -0,0 +1,37 @@
+# TODO
+
+## Fonctionnel
+
+-   Récupération des données via l'API
+-   Enregistrer la data de l'API & récupérer les données selon des paramètres
+
+---
+
+-   Bot -> cron task qui va lancer une stratégie en boucle, configurer les stratégies
+-   Strategy -> fonction qui va prendre en paramètre les données et retourner une ACTION (achat, vente, rien)
+-   Suivi des ACTIONs -> historique des ordres, carnet des actions
+-   Exposer les données via une API
+
+class Bot{
+strategies: Strategy[]
+
+    run(){
+        for(strategy in strategies){
+            if(strategy.shouldRun(timeElasped)){
+                strategy.run(timeElasped)
+            }
+        }
+    }
+
+}
+
+class MyStrategy extends Strategy{
+strategyConfig: StrategyConfig
+}
+
+## API
+
+-   Get les data du cours de X
+
+-   Les stratégies
+-   Pour chaque stratégies, l'historique des actions
--- src/routers/example.router.ts
+++ src/routers/example.router.ts
@@ -3,6 +3,8 @@ import { sendResponse } from "../utils/send-response";
 import { ResponseOkDto } from "../dto/responses/response-ok.dto";
 import { PaginatedDataDto } from "../dto/responses/paginated-data.dto";
 import { NotFoundError } from "../errors/not-found-error";
+import { validateDto } from "../dto/validate-dto";
+import { PaginationDto } from "../dto/requests/pagination.dto";
 
 const exampleRouter = Router();
 
@@ -12,8 +14,8 @@ const exampleRouter = Router();
  */
 
 interface User {
-  name: string;
-  age: number;
+    name: string;
+    age: number;
 }
 /**
  * The method sendReponse is used to send a response to the client.
@@ -21,84 +23,84 @@ interface User {
  *
  * NB: In the following case, the response does not contain any data other than the message and status code.
  */
-exampleRouter.get("/no-data", (req, res) => {
-  sendResponse(
-    res,
-    new ResponseOkDto("Some process was sucessfully performed...", 200)
-  );
+exampleRouter.get("/no-data", validateDto(PaginationDto), (req, res) => {
+    sendResponse(
+        res,
+        new ResponseOkDto("Some process was sucessfully performed...", 200)
+    );
 });
 
 /**
  * To send a successful response containing a single object, you can use the ResponseOkDto class like so:
  */
 exampleRouter.get("/single-object", (req, res) => {
-  const responseJsonObj: User = {
-    name: "John Doe",
-    age: 30,
-  };
+    const responseJsonObj: User = {
+        name: "John Doe",
+        age: 30,
+    };
 
-  sendResponse(
-    res,
-    new ResponseOkDto<User>(
-      "Single object retrieved successfully",
-      200,
-      responseJsonObj
-    )
-  );
+    sendResponse(
+        res,
+        new ResponseOkDto<User>(
+            "Single object retrieved successfully",
+            200,
+            responseJsonObj
+        )
+    );
 });
 
 /**
  * To send a successful response containing a list of objects, you can use the ResponseOkDto class like so:
  */
 exampleRouter.get("/list-of-objects", (req, res) => {
-  const responseJsonArr: User[] = [
-    {
-      name: "John Doe",
-      age: 30,
-    },
-    {
-      name: "Jane Doe",
-      age: 25,
-    },
-  ];
+    const responseJsonArr: User[] = [
+        {
+            name: "John Doe",
+            age: 30,
+        },
+        {
+            name: "Jane Doe",
+            age: 25,
+        },
+    ];
 
-  sendResponse(
-    res,
-    new ResponseOkDto<User[]>(
-      "List of objects retrieved successfully",
-      200,
-      responseJsonArr
-    )
-  );
+    sendResponse(
+        res,
+        new ResponseOkDto<User[]>(
+            "List of objects retrieved successfully",
+            200,
+            responseJsonArr
+        )
+    );
 });
 
 /**
  * To send a successful response containing a paginated list of objects, you can use the ResponseOkDto class like so:
  */
 exampleRouter.get("/page-of-objects", (req, res) => {
-  const responseJsonArr: User[] = [
-    {
-      name: "John Doe",
-      age: 30,
-    },
-    {
-      name: "Jane Doe",
-      age: 25,
-    },
-  ];
+    const responseJsonArr: User[] = [
+        {
+            name: "John Doe",
+            age: 30,
+        },
+        {
+            name: "Jane Doe",
+            age: 25,
+        },
+    ];
 
-  const page = 1;
-  const pageSize = 2;
-  const total = 100;
+    const page = 1;
+    const pageSize = 2;
+    const total = 100;
 
-  sendResponse(
-    res,
-    new ResponseOkDto<User>(
-      "Page of objects retrieved successfully",
-      200,
-      new PaginatedDataDto<User>(responseJsonArr, page, pageSize, total)
-    )
-  );
+    sendResponse(
+        res,
+        new ResponseOkDto<User>(
+            "Page of objects retrieved successfully",
+            200,
+            new PaginatedDataDto<User>(responseJsonArr, page, pageSize, total)
+        )
+    );
 });
 
 /**
@@ -107,7 +109,7 @@ exampleRouter.get("/page-of-objects", (req, res) => {
  * NB: The error middleware will catch this error and send a response to the client since it is thrown synchronously.
  */
 exampleRouter.get("/error", (req: Request, res: Response) => {
-  throw new NotFoundError("someUserName", "User not found", "username");
+    throw new NotFoundError("someUserName", "User not found", "username");
 });
 
 /**
@@ -116,14 +118,18 @@ exampleRouter.get("/error", (req: Request, res: Response) => {
  * NB: We need to catch the error and pass it to the next function to be handled by the error middleware since it is thrown asynchronously.
  */
 exampleRouter.get(
-  "/error-async",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      throw new NotFoundError("someUserName", "User not found", "username");
-    } catch (error: any) {
-      next(error);
+    "/error-async",
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            throw new NotFoundError(
+                "someUserName",
+                "User not found",
+                "username"
+            );
+        } catch (error: any) {
+            next(error);
+        }
     }
-  }
 );
 
 /**
@@ -133,23 +139,23 @@ exampleRouter.get(
  *
  */
 exampleRouter.get(
-  "/server-error-sim-untrusted",
-  async (req: Request, res: Response, next: NextFunction) => {
-    setTimeout(() => {
-      throw new Error("Server error");
-    }, 1000);
-    sendResponse(res, new ResponseOkDto("Sim OK", 200));
-  }
+    "/server-error-sim-untrusted",
+    async (req: Request, res: Response, next: NextFunction) => {
+        setTimeout(() => {
+            throw new Error("Server error");
+        }, 1000);
+        sendResponse(res, new ResponseOkDto("Sim OK", 200));
+    }
 );
 
 exampleRouter.get(
-  "/server-error-sim-trusted",
-  async (req: Request, res: Response, next: NextFunction) => {
-    setTimeout(() => {
-      throw new NotFoundError("User", "User not found", "username");
-    }, 1000);
-    sendResponse(res, new ResponseOkDto("Sim OK", 200));
-  }
+    "/server-error-sim-trusted",
+    async (req: Request, res: Response, next: NextFunction) => {
+        setTimeout(() => {
+            throw new NotFoundError("User", "User not found", "username");
+        }, 1000);
+        sendResponse(res, new ResponseOkDto("Sim OK", 200));
+    }
 );
 
 export default exampleRouter;
