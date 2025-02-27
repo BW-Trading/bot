d411bfe04a2f60b8d46cd6d006fe8c15fac2618e
William Isabelle <william.isabelle@etu.univ-grenoble-alpes.fr>
2025-02-27 02:24:00 +0100

    fix: order book limit param as optionnal

--- src/dto/requests/market-data/order-book.dto.ts
+++ src/dto/requests/market-data/order-book.dto.ts
@@ -1,10 +1,11 @@
-import { IsNumberString, IsString } from "class-validator";
+import { IsNumberString, IsOptional, IsString } from "class-validator";
 import { DTO } from "../../dto";
 
 export class OrderBookDto extends DTO {
     @IsString()
     symbol!: string;
 
+    @IsOptional()
     @IsNumberString()
     limit?: number;
 }
