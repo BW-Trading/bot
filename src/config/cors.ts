2698716278e7955f93234e851d3a5ba532f8a93a
Bracquib <benjaminbracquier@gmail.com>
2025-03-07 13:36:55 +0100

    rectification sur cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,11 +2,4 @@ import cors from "cors";
 
 export const corsConfig = cors({
     credentials: true,
-    allowedHeaders: [
-        "Origin",
-        "X-Requested-With",
-        "Content-Type",
-        "Accept",
-        "Authorization",
-    ],
 });
