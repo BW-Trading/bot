1893db2a42989170269f2fd818115feb25399ab2
William Isabelle <william.isabelle@etu.univ-grenoble-alpes.fr>
2025-03-17 22:44:21 +0100

    update rate limiter

--- src/config/rate-limiter.ts
+++ src/config/rate-limiter.ts
@@ -2,6 +2,6 @@ import rateLimit from "express-rate-limit";
 
 export const rateLimiterConfig = rateLimit({
     limit: 50,
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 60 * 1000, // 15 minutes
     standardHeaders: "draft-7",
 });
